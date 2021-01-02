2a58079861516f1a848c9c15b5fe5e123139db24
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-02 15:52:19 -0400

    Validar actualizacion de usuario para el mismo usuario sin necesidad de ser admin

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -46,7 +46,36 @@ const validarAdmin_ROLE= async(req=request, res=response,next)=>{
         })
     }
 }
+const validarAdmin_ROLE_o_MismoUsuario= async(req=request, res=response,next)=>{
+    const uid = req.uid;
+    const id =req.params['id'];
+    try {
+        const usuarioDB = await Usuario.findById(uid);
+        if(!usuarioDB){
+            return res.status(404).json({
+                ok:false,
+                msg:'Usuario no existe'
+            })
+        }
+        if(usuarioDB.role ==='ADMIN_ROLE' || uid === id){
+            next();
+        }
+        else{
+            return res.status(403).json({
+                ok:false,
+                msg:'No tiene privilegios para hacer eso'
+            }) 
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg:'Error inesperado... revisar logs'
+        })
+    }
+}
 module.exports = {
     validarJWT,
-    validarAdmin_ROLE
+    validarAdmin_ROLE,
+    validarAdmin_ROLE_o_MismoUsuario
 }
